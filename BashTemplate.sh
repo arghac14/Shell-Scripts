82aa0a3cb6046619f8957e47efb7a20db542e847
Arghadip Chakraborty <chakrabortyarghadip5@gmail.com>
2019-02-11 23:29:19 +0530

    Generates your new bash scripts and append '#! bin/sh' to it.

---
+++ BashTemplate.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+echo -ne "Enter the name of your bash script: "
+read script
+if [ -f $script.sh ];then 
+   echo "File already exists!"
+   exit 1
+fi
+echo "!# bin/sh" >> $script.sh
+nano $script.sh
